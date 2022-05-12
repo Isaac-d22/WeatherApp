f4f8493481e596ba819857454aa76c4f0bd1067e
id373 <id373@cam.ac.uk>
2022-05-12 16:02:57 +0100

    Attempt to sort out background colour
    Partially working background colour need to stop the overwriting

--- src/main/java/com/example/weatherapp/HelloApplication.java
+++ src/main/java/com/example/weatherapp/HelloApplication.java
@@ -3,7 +3,9 @@ package com.example.weatherapp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.time.LocalTime;
 
 import java.io.IOException;
 
@@ -11,13 +13,28 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 365, 650);
         stage.setTitle("Hello!");
+        stage.centerOnScreen();
+        setBackground(scene);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public static void setBackground (Scene scene){
+        LocalTime start = LocalTime.parse( "09:00:00" );
+        LocalTime stop = LocalTime.parse( "21:00:00" );
+        LocalTime current = LocalTime.now();
+
+        if (current.isAfter(start) && current.isBefore(stop)){
+            System.out.println("Working");
+            scene.setFill(Color.web("#010128"));
+        }
+
+        scene.setFill(Color.web("#77C7EC"));
     }
 }
