0bae265cd306b58535686c67f9e4b6e6ac4082bd
id373 <id373@cam.ac.uk>
2022-05-17 15:01:08 +0100

    Partial settings page implementation

--- src/main/java/com/example/weatherapp/SettingsController.java
+++ src/main/java/com/example/weatherapp/SettingsController.java
@@ -6,14 +6,32 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 
+import java.time.LocalTime;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Pane;
+import java.time.LocalTime;
+
+import java.time.LocalTime;
+
 public class SettingsController {
     @FXML
-    private Pane backgroundPane;
+    private AnchorPane background;
+    @FXML
+    private AnchorPane page;
     @FXML
     private Label currentTemperature;
 
-    public void setBackgroundColor(boolean isDay) {
-        backgroundPane.setStyle("-fx-background-color: #" + (isDay? "3e91cd" : "0b1924"));
+    @FXML
+    private void initialize() {
+        LocalTime start = LocalTime.parse("09:00:00");
+        LocalTime stop = LocalTime.parse("21:00:00");
+        LocalTime current = LocalTime.now();
+        background.setStyle("-fx-background-color: #" + ((current.isAfter(start) && current.isBefore(stop))? "3e91cd" : "0b1924"));
     }
 
     @FXML
