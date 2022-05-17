a16fe5564de401be5f33a1e8f29e45e29af51376
Jessica Drury <jessica@drurys.org>
2022-05-17 16:01:10 +0100

    Added to Route and Location classes

--- src/main/java/com/example/weatherapp/Location.java
+++ src/main/java/com/example/weatherapp/Location.java
@@ -1,9 +1,18 @@
 package com.example.weatherapp;
 
+import java.time.LocalTime;
+
 public class Location {
     String address;
     double latitude;
     double longitude;
+    LocalTime time;
 
+    Location(String address) {
+        this.address = address;
+    }
 
+    void refreshWeather() {
+
+    }
 }
