c72297ad6db20fef76ad5bd3157887621d56c8f9
Luca Choteborsky <lc869@cam.ac.uk>
2022-05-17 11:39:12 +0100

    Implemented the API call thingy

--- src/main/java/com/example/weatherapp/OWM/ApiCaller.java
+++ src/main/java/com/example/weatherapp/OWM/ApiCaller.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp.OWM;
 
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.ResponseBody;
@@ -28,12 +29,15 @@ public class ApiCaller {
         //System.out.println(response.string());
         Gson gson = new Gson();
         ApiResponse json = gson.fromJson(response.string(), ApiResponse.class);
-        System.out.println(json.base); //Seems to be a rounding error
+        System.out.println(json.main.get("feels_like")); //Seems to be a rounding error
+        //JsonObject json = gson.fromJson(response.string(), JsonObject.class);
+        System.out.println(json.sys.get("sunrise"));
     }
 }
 
-//{"coord":{"lon":139,"lat":35},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10d"}],
-// "base":"stations","main":{"temp":286.66,"feels_like":286.57,"temp_min":286.66,"temp_max":286.66,"pressure":1010,
-// "humidity":96},"visibility":6676,"wind":{"speed":1.95,"deg":45,"gust":2.72},"rain":{"1h":0.56},"clouds":{"all":100},
-// "dt":1652647599,"sys":{"type":2,"id":2019346,"country":"JP","sunrise":1652643613,"sunset":1652694041},
+//{"coord":{"lon":139,"lat":35},"weather":[{"id":500,"main":"Rain","description":"light rain","icon":"10n"}]
+// ,"base":"stations","main":{"temp":286.1,"feels_like":285.96,"temp_min":286.1,"temp_max":286.1,
+// "pressure":1016,"humidity":96},"visibility":10000,"wind":{"speed":0.32,"deg":320,"gust":0.7},
+// "rain":{"1h":0.11},"clouds":{"all":100},"dt":1652783611,
+// "sys":{"type":2,"id":2019346,"country":"JP","sunrise":1652729970,"sunset":1652780488},
 // "timezone":32400,"id":1851632,"name":"Shuzenji","cod":200}
