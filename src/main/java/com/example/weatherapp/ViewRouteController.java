273433919268cfc9e531ad50c68314c51fffd6b8
Cameron Crawford <61328266+CameronLCrawford@users.noreply.github.com>
2022-05-19 15:09:42 +0100

    Added load route.

--- src/main/java/com/example/weatherapp/ViewRouteController.java
+++ src/main/java/com/example/weatherapp/ViewRouteController.java
@@ -1,18 +1,26 @@
 package com.example.weatherapp;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class ViewRouteController implements IPageController{
     private AppController app;
     @FXML
     private AnchorPane background;
+    @FXML
+    private TableView locationTable;
+    @FXML
+    private Button loadResults;
 
     @FXML
     private void initialize() {
@@ -32,4 +40,26 @@ public class ViewRouteController implements IPageController{
     public void setApp(AppController app) {
         this.app = app;
     }
+
+    public void loadRoute(MouseEvent mouseEvent) {
+        ArrayList<WeatherInfo> weatherInfo;
+        for (String location : app.locations) {
+            WeatherInfo info = new WeatherInfo();
+            info.placeName = location;
+            location = location.replaceAll(" ", "%20");
+            location = location.replaceAll("'", "%27");
+            location = location.replaceAll(",", "%2C");
+            WeatherApiResponse weatherAtLocation = ApiCaller.getStatsAtStreetName(location);
+            //info.temperature = weatherAtLocation.temperature;
+            //info.rainChance = weatherAtLocation.rainChance;
+            //info.windSpeed = weatherAtLocation.windSpeed;
+        }
+    }
+}
+
+class WeatherInfo {
+    String placeName;
+    String temperature;
+    String rainChance;
+    String windSpeed;
 }
